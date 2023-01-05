ddd8d739a0530bad79c18d89206010e909b1334e
Radosław Kluczewski <radek.info.klucz@gmail.com>
2023-01-05 18:27:57 +0100

    Add backgrounds

--- src/main/java/agh/project/GUI/GuiElement.java
+++ src/main/java/agh/project/GUI/GuiElement.java
@@ -8,15 +8,16 @@ import agh.project.Interfaces.IWorldMap;
 import javafx.geometry.HPos;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 
+import java.awt.*;
 import java.io.*;
 
 import static java.lang.System.out;
@@ -125,42 +126,54 @@ public class GuiElement extends AbstractWorldMap {
         Label widthLabel = new Label("Width of map: ");
         TextField widthText = new TextField();
         widthText.setText(String.valueOf(width));
+        widthLabel.setTextFill(Color.WHITE);
+
 
         Label heightLabel = new Label("Height of map: ");
         TextField heightText = new TextField();
         heightText.setText(String.valueOf(height));
+        heightLabel.setTextFill(Color.WHITE);
+
 
         Label amountOfAnimalsLabel = new Label("Amount of start animals: ");
         TextField amountOfStartAnimalsText = new TextField();
         amountOfStartAnimalsText.setText(String.valueOf(startAnimals));
+        amountOfAnimalsLabel.setTextFill(Color.WHITE);
 
         Label amountOfPlantsLabel = new Label("Amount of start plants: ");
         TextField amountOfStartPlantsText = new TextField();
         amountOfStartPlantsText.setText(String.valueOf(startPlants));
+        amountOfPlantsLabel.setTextFill(Color.WHITE);
 
         Label startEnergyLabel = new Label("Start animal energy: ");
         TextField startAnimalEnergyText = new TextField();
         startAnimalEnergyText.setText(String.valueOf(startEnergy));
+        startEnergyLabel.setTextFill(Color.WHITE);
 
         Label moveEnergyLabel = new Label("Move energy: ");
         TextField moveEnergyText = new TextField();
         moveEnergyText.setText(String.valueOf(moveEnergy));
+        moveEnergyLabel.setTextFill(Color.WHITE);
 
         Label energyFromEatingLabel = new Label("Energy from eating: ");
         TextField energyFromEatingText = new TextField();
         energyFromEatingText.setText(String.valueOf(energyFromEating));
+        energyFromEatingLabel.setTextFill(Color.WHITE);
 
         Label numberOfNewDailyPlantsLabel = new Label("Daily new plant number: ");
         TextField numberOfNewDailyPlantsText = new TextField();
         numberOfNewDailyPlantsText.setText(String.valueOf(dailyPlants));
+        numberOfNewDailyPlantsLabel.setTextFill(Color.WHITE);
 
         Label neededEnergyToCopulateLabel = new Label("Energy to copulate: ");
         TextField neededEnergyToCopulateText = new TextField();
         neededEnergyToCopulateText.setText(String.valueOf(energyToCopulate));
+        neededEnergyToCopulateLabel.setTextFill(Color.WHITE);
 
         Label gensLenghtLabel = new Label("Gens length: ");
         TextField gensLengthText = new TextField();
         gensLengthText.setText(String.valueOf(gensLength));
+        gensLenghtLabel.setTextFill(Color.WHITE);
 
 
         VBox controls = new VBox(new HBox(widthLabel, widthText), new HBox(heightLabel, heightText),
@@ -190,10 +203,15 @@ public class GuiElement extends AbstractWorldMap {
         brownAnimal.setFitHeight(15);
         brownAnimal.setFitWidth(15);
         Label redLabel = new Label("  Actual energy < 0.3");
+        redLabel.setTextFill(Color.WHITE);
         Label orangeLabel = new Label("  0.3 < Actual energy <= 0.5");
+        orangeLabel.setTextFill(Color.WHITE);
         Label lightOrangeLabel = new Label("  0.5 < Actual energy <= 0.7");
+        lightOrangeLabel.setTextFill(Color.WHITE);
         Label goldLabel = new Label("  0.7 < Actual energy <= Start energy");
+        goldLabel.setTextFill(Color.WHITE);
         Label brownLabel = new Label("  Actual energy > Start energy");
+        brownLabel.setTextFill(Color.WHITE);
 
         VBox description = new VBox(new HBox(redAnimal, redLabel), new HBox(orangeAnimal, orangeLabel),
                 new HBox(lightOrangeAnimal, lightOrangeLabel), new HBox(goldAnimal, goldLabel),
@@ -203,13 +221,31 @@ public class GuiElement extends AbstractWorldMap {
 
     public void openMapWindow(Stage primaryStage, HBox startSimulation, VBox createDescription, String title) {
         Stage mapWindow = new Stage();
+        Image imageBackground;
         if (title.equals("Earth Map")) {
             mapWindow.setTitle("Earth Map");
+            imageBackground = new Image("file:src/main/resources/Earth.jpg");
         } else {
             mapWindow.setTitle("Hell Map");
+            imageBackground = new Image("file:src/main/resources/Hell.jpg");
         }
 
         StackPane secondaryLayout  = new StackPane();
+        BackgroundSize backgroundSize = new BackgroundSize(
+                BackgroundSize.AUTO,
+                BackgroundSize.AUTO,
+                true,
+                true,
+                true,
+                true);
+
+        secondaryLayout.setBackground(new Background(new BackgroundImage(
+                imageBackground,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.DEFAULT,
+                backgroundSize)));
+
         VBox vBox = new VBox();
         vBox.getChildren().addAll(buttonsForMaps(), startSimulation, createDescription);
         secondaryLayout.getChildren().addAll(vBox);
